6463714c132a35efb6f5d5f2411a379a98cc830b
Ben <892156860@qq.com>
2017-12-06 16:29:44 +0800

    模型设计

--- daydayfresh/daydayfresh/urls.py
+++ daydayfresh/daydayfresh/urls.py
@@ -17,6 +17,10 @@ from django.conf.urls import url
 
 import xadmin
 
+from daydayfresh.settings import MEDIA_ROOT
+from django.views.static import serve
+
 urlpatterns = [
     url(r'^admin/', xadmin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT})
 ]
--- daydayfresh/db_tools/data/category_data.py
+++ daydayfresh/db_tools/data/category_data.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
 row_data = [
     {
         'sub_categorys': [
